36b1cbdbda4f9756928171f2cf54f1fea44efa9e
Mikhail <314159265absurdus@gmail.com>
2018-05-08 15:54:02 +0300

    Added metrika code

--- spasibo.php
+++ spasibo.php
@@ -1,4 +1,42 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ремонт | Установка кондиционеров в Москве по доступным ценам - Спасибо</title>
+</head>
+<body>
+	<h1>
+		<?php echo "Спасибо! Ваше сообщение отправлено."; ?>
+	</h1>
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+	    (function (d, w, c) {
+	        (w[c] = w[c] || []).push(function() {
+	            try {
+	                w.yaCounter48789770 = new Ya.Metrika({
+	                    id:48789770,
+	                    clickmap:true,
+	                    trackLinks:true,
+	                    accurateTrackBounce:true,
+	                    webvisor:true
+	                });
+	            } catch(e) { }
+	        });
 
-echo "Спасибо! Ваше сообщение отправлено.";
- ?>
+	        var n = d.getElementsByTagName("script")[0],
+	            s = d.createElement("script"),
+	            f = function () { n.parentNode.insertBefore(s, n); };
+	        s.type = "text/javascript";
+	        s.async = true;
+	        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+	        if (w.opera == "[object Opera]") {
+	            d.addEventListener("DOMContentLoaded", f, false);
+	        } else { f(); }
+	    })(document, window, "yandex_metrika_callbacks");
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/48789770" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->	
+</body>
+</html>
+
